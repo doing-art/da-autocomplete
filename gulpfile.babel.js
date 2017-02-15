99800fd9efe1f8b755e4e2f6f232af1563e29a60
Paramonov_Nick <paramonov.work@gmail.com>
2017-02-15 23:17:58 +0300

    Add gulpfile

---
+++ gulpfile.babel.js
@@ -0,0 +1,16 @@
+import gulp from 'gulp'
+import webpack from 'webpack-stream'
+import sourcemaps from 'gulp-sourcemaps'
+import rename from 'gulp-rename'
+import uglify from 'gulp-uglify'
+
+gulp.task('build', () => {
+    return gulp.src('./src/javascript/index.js')
+        .pipe(webpack(require('./webpack.config.js')))
+        .pipe(gulp.dest('./lib/js'))
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify({ preserveComments: 'license' }))
+        .pipe(rename('da-autocomplete.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./lib/js'));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
-module.exports = {
-    entry: './src/javascript/index.js',
+import webpack from "webpack";
+import pkg from "./package.json";
+
+export default {
     output: {
-        library: 'daAutocomplete',
+        library: pkg.name,
         libraryTarget: "umd",
-        filename: "lib/js/da-autocomplete.js"
+        filename: `${pkg.name}.js`
     },
+    devtool: "#inline-source-map",
     module: {
         loaders: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader",
+                loader: 'babel-loader',
                 query: {
-                    compact: false // because I want readable output
+                    compact: false
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.BannerPlugin(`
+           ${pkg.name} - ${pkg.description}
+           Author: ${pkg.author}
+           Version: v${pkg.version}
+           Url: ${pkg.homepage}
+           License(s): ${pkg.license}
+        `)
+    ]
 };
