a951e4014ebada41b5f7cea7ad810e404310b2a2
Paramonov_Nick <paramonov.work@gmail.com>
2017-03-19 08:29:45 +0300

    Clear packages, remove gulp

--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp'
-import webpack from 'webpack-stream'
-
-gulp.task('build', () => {
-    return gulp.src('./src/javascript/index.js')
-        .pipe(webpack(require('./webpack.config.js')))
-        .pipe(gulp.dest('./lib/js'));
-});
